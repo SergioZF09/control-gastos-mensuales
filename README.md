9433401806954b1132958cddbc432388ee622045
SergioFraga <sergiozfraga@gmail.com>
2024-09-22 17:02:56 -0700

    proyecto terminado

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# control-gastos-mensuales
-Este es un proyecto realizado en el curso Inmersión Dev con JavaScript de Alura Latam. Se trata de un generador de un control de gastos mensuales, donde se introduce el nombre del gasto, su descripción y su valor.
+# Generador de Contraseña
+
+## Descripción
+Este es un proyecto realizado en el curso Inmersión Dev con JavaScript de Alura Latam junto con los Maestros Christian Velasco, Leonardo Jose Castillo Lacruz y Jeanmarie Quijada, e incluyendo tres retos para realizar.
+
+Se trata de un generador de un control de gastos mensuales, donde se introduce el nombre del gasto, su descripción y su valor, después se genera una lista mostrando dicha información junto con dos botones: editar y eliminar, y muestra el total de todos los gastos agregados.
+
+## Funciones del proyecto
+- **Campo Nombre del Gasto.** Se introduce el nombre del gasto que se realizó.
+
+- **Campo Descripción del Gasto.** Se introduce una descripción más detallada sobre el gasto que se realizó.
+
+- **Campo Valor del Gasto.** Se introduce el valor del gasto que se realizó en dólares.
+
+- **Botón Agregar Gasto.** Se da click para agregar la información de los campos y mostrarlos en una lista en la parte de abajo.
+
+- **Botón Editar.** Se da click para editar la información de un gasto en específico.
+
+- **Botón Editar Gasto.** Se da click para guardar los cambios del gasto que se editó.
+
+- **Botón Eliminar.** Se da click para elimiar un gasto en específico.
+
+- **Mensaje para los campos Nombre del Gasto, Descripción del Gasto y Valor del Gasto al agregar un gasto.** Al los tres campos junto muestra tres mensajes.
+
+    - Cuando no se introduce información en los tres campos muestra lo siguiente: "**_Por favor, llene todos los campos._**".
+
+    - Cuando se agrega un gasto muestra lo siguiente: "**_Gasto agregado._**".
+
+    - Cuando se agrega un gasto con un valor mayor a 150 muestra lo siguiente: "**_Se registró un gasto mayor a USD 150._**".
+
+- **Mensaje para los campos Nombre del Gasto, Descripción del Gasto y Valor del Gasto al editar un gasto.** Al agregar un gasto se muestra el botón Editar, se inserta la información a los campos y muestra dos mensajes (al darle click al botón Editar Gasto).
+
+    - Cuando no se introduce información en los tres campos muestra lo siguiente: "**_Por favor, llene todos los campos._**".
+
+    - Cuando se edita un gasto muestra lo siguiente: "**_Gasto editado._**".
+
+- **Mensaje para borrar un gasto.** Al agregar un gasto se muestra el botón Eliminar y muestra un mensaje.
+
+    - Cuando se le da click al botón Eliminar muestra lo siguiente: "**_Gasto eliminado._**".
+
+## Tecnologías
+- HTML5
+
+- CSS3
+
+- JavaScript
+
+## Desafíos
+- Generar un mensaje de alerta cuando se registre un gasto mayor a 150$ dólares.
+
+- Agregar un nuevo campo donde se pueda colocar una descripción más detallada del gasto.
+
+- Agregar un botón que permita modificar los gastos registrados.
+
+## Autor
+Sergio Zúñiga Fraga - Licenciado en Informática
---
+++ assets/js/script.js
@@ -0,0 +1,206 @@
+let listaNombresGastos = [];
+let listaDescripcionesGastos = [];
+let listaValoresGastos = [];
+let gastoMayor = 150;
+
+//Esta función se invoca al momento de que el usuario hace clcik en el boton Agregar
+function clickBotonAgregar() {
+    let nombreGasto = document.getElementById("nombreGasto").value;
+    let descripcionGasto = document.getElementById("descripcionGasto").value;
+    let valorGasto = document.getElementById("valorGasto").value;
+    
+    //Valida los campos. Si están vacios uno de ellos o todos, manda mensaje de error, en caso contrario envía la información
+    if (nombreGasto === "" || descripcionGasto === "" || valorGasto === "") {
+
+        //Muestra mensaje para indicar que se llenen los campos
+        Swal.fire({
+            title: '¡Ocurrió un error!',
+            text: `Por favor, llene todos los campos.`,
+            icon: 'error',
+            confirmButtonText: 'Continuar'
+        })
+        //No almacena y muestra la información con los campos incompletos
+        return;
+        
+    } else {
+
+        //Valida si el gasto ingresado fue mayor que 150
+        if (valorGasto > gastoMayor) {
+            //Muestra mensaje para indicar un gasto mayor a 150 dólares
+            Swal.fire({
+                title: '¡Ten cuidado!',
+                text: `Se registró un gasto mayor a USD ${gastoMayor}`,
+                icon: 'warning',
+                confirmButtonText: 'Continuar'
+            })
+        }
+
+        //Almacena la información de los campos a las variables listas de cada campo
+        listaNombresGastos.push(nombreGasto);
+        listaDescripcionesGastos.push(descripcionGasto);
+        listaValoresGastos.push(valorGasto);
+
+        //Muestra mensaje para indicar que se agregó el gasto
+        Swal.fire({
+            title: '¡Listo!',
+            text: `Gasto agregado.`,
+            icon: 'success',
+            confirmButtonText: 'Continuar'
+        })
+        
+        //Muestra la información de las listas
+        actualizarListaGastos();
+
+    }
+}
+
+//Esta función se invoca al momento de que el usuario hace la acción de agregar información
+function actualizarListaGastos() {
+
+    const listaElementos = document.getElementById("listaDeGastos");
+
+    const totalElementos = document.getElementById("totalGastos");
+
+    let htmlLista = "";
+
+    let totalGastos = 0;
+
+    //Recorre la variable lista de nombres de los gastos con cada elemento y su posición para mostrar todos los gastos
+    //y se usan las etiquetas ul li para ponerlas como listas
+    listaNombresGastos.forEach((elemento, posicion) => {
+        const valorGasto = Number(listaValoresGastos[posicion]);
+        const descripcionGasto = listaDescripcionesGastos[posicion];
+        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)}
+                        <br> ${descripcionGasto}
+                        <button class="editarGasto" onclick="editarGasto(${posicion});">Editar</button>
+                        <button class="eliminarGasto" onclick="eliminarGasto(${posicion});">Eliminar</button>
+                        </li>`;
+        //Calculamos el total de gastos
+        totalGastos += Number(valorGasto);
+    });
+
+    listaElementos.innerHTML = htmlLista;
+    totalElementos.innerHTML = totalGastos.toFixed(2);
+
+    //Limpia todos los campos
+    limpiar();
+}
+
+//Esta función se invoca al momento de que el usuario hace la acción de agregar información
+function limpiar() {
+    
+    //Al momento de agregar la información, se elimina dicha información de los campos
+    document.getElementById("nombreGasto").value = "";
+    document.getElementById("descripcionGasto").value = "";
+    document.getElementById("valorGasto").value = "";
+
+}
+
+//Esta función se invoca al momento de que el usuario hace clcik en el boton Eliminar
+function eliminarGasto(posicion) {
+    
+    //Elimina el gasto dependiendo de la posición en la que esté en la variable lista de cada campo
+    listaNombresGastos.splice(posicion, 1);
+    listaValoresGastos.splice(posicion, 1);
+
+    //Muestra mensaje para indicar que se eliminó un gasto
+    Swal.fire({
+        title: '¡Listo!',
+        text: `Gasto eliminado.`,
+        icon: 'success',
+        confirmButtonText: 'Continuar'
+    })
+
+    //Actualiza y muestra la información de las listas
+    actualizarListaGastos();
+
+}
+
+//Esta función se invoca al momento de que el usuario hace clcik en el boton Editar
+function editarGasto(posicion) {
+    
+    //La información que está en el listado ahora se muestra en los campos para editarlas
+    document.getElementById("nombreGasto").value = listaNombresGastos[posicion];
+    document.getElementById("descripcionGasto").value = listaDescripcionesGastos[posicion];
+    document.getElementById("valorGasto").value = listaValoresGastos[posicion];
+
+    //Se actualiza la posición para la nueva información introducida
+    nuevaPosicion = posicion;
+
+    //Muestra el botón Editar Gasto y se oculta el botón Agregar Gasto
+    mostrarBotonEditar();
+
+}
+
+//Esta función se invoca al momento de que el usuario hace clcik en el boton Editar Gasto
+function clickBotonEditar() {
+
+    const editarNombreGasto = document.getElementById("nombreGasto").value;
+    const editarDescripcionGasto = document.getElementById("descripcionGasto").value;
+    const editarValorGasto = document.getElementById("valorGasto").value;
+
+    //Valida los campos. Si están vacios uno de ellos o todos, manda mensaje de error, en caso contrario envía la información
+    if (editarNombreGasto === "" || editarDescripcionGasto === "" || editarValorGasto === "") {
+
+        //Muestra mensaje para indicar que se llenen los campos
+        Swal.fire({
+            title: '¡Ocurrió un error!',
+            text: `Por favor, llene todos los campos.`,
+            icon: 'error',
+            confirmButtonText: 'Continuar'
+        })
+        //No almacena y muestra la información con los campos incompletos
+        return;
+        
+    } else {
+
+        //La nueva información introducida se agrega a las variables listas con la nueva posición
+        listaNombresGastos[nuevaPosicion] = editarNombreGasto;
+        listaDescripcionesGastos[nuevaPosicion] = editarDescripcionGasto;
+        listaValoresGastos[nuevaPosicion] = editarValorGasto;
+
+        //Muestra mensaje para indicar que se editó un gasto
+        Swal.fire({
+            title: '¡Listo!',
+            text: `Gasto editado.`,
+            icon: 'success',
+            confirmButtonText: 'Continuar'
+        })
+
+        //Actualiza y muestra la información de las listas
+        actualizarListaGastos();
+
+        //Muestra el botón Editar Gasto y se oculta el botón Agregar Gasto
+        mostrarBotonAgregar();
+
+    }
+
+}
+
+//Esta función se invoca al momento de empezar la acción de editar información
+function mostrarBotonEditar() {
+
+    //Se obtienen el control de los dos botones
+    const botonAgregar = document.querySelector("#botonAgregar");
+    const botonEditar = document.querySelector("#botonEditar");
+
+    //Al botón Agregar Gasto se le añade el atributo hidden para ocultarlo
+    //Al botón Editar Gasto se le quita el atributo hidden para mostrarlo
+    botonAgregar.setAttribute("hidden", "");
+    botonEditar.removeAttribute("hidden");
+
+}
+
+//Esta función se invoca al momento de finalizar la acción de editar información
+function mostrarBotonAgregar() {
+
+    //Se obtienen el control de los dos botones
+    const botonAgregar = document.querySelector("#botonAgregar");
+    const botonEditar = document.querySelector("#botonEditar");
+
+    //Al botón Agregar Gasto se le quita el atributo hidden para mostrarlo
+    //Al botón Editar Gasto se le añade el atributo hidden para ocultarlo
+    botonAgregar.removeAttribute("hidden");
+    botonEditar.setAttribute("hidden", "");
+
+}
